7f1e07bcc176dea4890a274b0e1a6dbec46e24cc
clare29 <1397452342@qq.com>
2018-01-16 15:23:54 +0800

    updata

--- static/js/app/controller/user/AdvertiseETH.js
+++ static/js/app/controller/user/AdvertiseETH.js
@@ -107,7 +107,7 @@ define([
 					<td class="createDatetime">${base.formateDatetime(item.createDatetime)}</td>
 					<td class="status">${adsStatusValueList[item.status]}</td>
 					<td class="operation">
-						<div class="am-button goHref" data-href="../trade/sell-detail.html?adsCode=${item.code}">查看详情</div>
+						<div class="am-button goHref" data-href="../trade/sell-detail.html?code=${item.code}">查看详情</div>
 									</td>
 				</tr>`
         }
