e53aeb716c3e3251536a74082efe803b2a358cfb
hss1994 <>
2018-10-17 02:36:41 +0800

    updata

--- static/js/app/controller/Top.js
+++ static/js/app/controller/Top.js
@@ -62,98 +62,98 @@ define([
     		var tmpl = coinList[coinListKey[i]]
     		buyListHtml+=`<li class="goHref" data-href="../trade/buy-list.html?coin=${tmpl.coin.toLowerCase()}">${tmpl.coin}</li>`;
     		sellListHtml += `<li class="goHref" data-href="../trade/sell-list.html?coin=${tmpl.coin.toLowerCase()}">${tmpl.coin}</li>`;
-    		advListHtml += `<li class="goHref" data-href="../trade/advertise.html?coin=${tmpl.coin.toLowerCase()}">${tmpl.coin}</li>`;
-    	}
-    	
-    	//购买
-    	$(".head-nav-wrap .buy .down-wrap ul").html(buyListHtml);
-    	//购买
-    	$(".head-nav-wrap .sell .down-wrap ul").html(sellListHtml);
-    	//购买
-    	$(".head-nav-wrap .advertise .down-wrap ul").html(advListHtml);
+            advListHtml += `<li class="coinItem" data-href="../trade/advertise.html?coin=${tmpl.coin.toLowerCase()}">${tmpl.coin}</li>`;
+        }
+
+        //购买
+        $(".head-nav-wrap .buy .down-wrap ul").html(buyListHtml);
+        //购买
+        $(".head-nav-wrap .sell .down-wrap ul").html(sellListHtml);
+        //购买
+        $(".head-nav-wrap .advertise .down-wrap ul").html(advListHtml);
     }
-    
+
     // 获取banner
     function getBanner(){
         return GeneralCtr.getBanner({}).then((data) => {
-        	data.forEach((item) => {
-        		// if (item.location === 'web_download') {
-	            // 	$('#downImg').attr("src",base.getPic(item.pic,"?imageMogr2/auto-orient/thumbnail/!280x280r"));
-	        	// } else
-				if (item.location === 'web_qq') {
-	            	$('#qqImg').attr("src",base.getPic(item.pic,"?imageMogr2/auto-orient/thumbnail/!280x280r"));
-	        	} else if (item.location === 'web_weibo') {
-	            	$('#wbImg').attr("src",base.getPic(item.pic,"?imageMogr2/auto-orient/thumbnail/!280x280r"));
-	        	} else if (item.location === 'web_wechat') {
-	            	$('#wxImg').attr("src",base.getPic(item.pic,"?imageMogr2/auto-orient/thumbnail/!280x280r"));
-	        	} else if (item.location === 'web_trade') {
-	            	$('#tradeBanner').css("background-image","url('"+base.getPic(item.pic,"?imageMogr2/auto-orient/thumbnail/!1200x90r")+"')");
-	        	}
-        	})
+            data.forEach((item) => {
+                // if (item.location === 'web_download') {
+                // 	$('#downImg').attr("src",base.getPic(item.pic,"?imageMogr2/auto-orient/thumbnail/!280x280r"));
+                // } else
+                if (item.location === 'web_qq') {
+                    $('#qqImg').attr("src",base.getPic(item.pic,"?imageMogr2/auto-orient/thumbnail/!280x280r"));
+                } else if (item.location === 'web_weibo') {
+                    $('#wbImg').attr("src",base.getPic(item.pic,"?imageMogr2/auto-orient/thumbnail/!280x280r"));
+                } else if (item.location === 'web_wechat') {
+                    $('#wxImg').attr("src",base.getPic(item.pic,"?imageMogr2/auto-orient/thumbnail/!280x280r"));
+                } else if (item.location === 'web_trade') {
+                    $('#tradeBanner').css("background-image","url('"+base.getPic(item.pic,"?imageMogr2/auto-orient/thumbnail/!1200x90r")+"')");
+                }
+            })
         }, (msg) => {
             base.showMsg(msg || "加载失败");
         });
     }
-    
+
     //我的账户
     function getAccount(){
-    	return AccountCtr.getAccount().then((data)=>{
-    		var htmlAccount = '';
-    		var html = '';
-    		data.accountList.forEach(function(item, i){
-    			
-    			if(i<3){
-    				//判断币种是否发布
-	    			if(base.getCoinCoin(item.currency)){
-	    				htmlAccount +=`<p>${item.currency}：<samp>${base.formatMoney(item.amountString,'',item.currency)}</samp></p>`;
-	    			
-		    			html += `<div class="list ${item.currency.toLocaleLowerCase()}">
+        return AccountCtr.getAccount().then((data)=>{
+            var htmlAccount = '';
+            var html = '';
+            data.accountList.forEach(function(item, i){
+
+                if(i<3){
+                    //判断币种是否发布
+                    if(base.getCoinCoin(item.currency)){
+                        htmlAccount +=`<p>${item.currency}：<samp>${base.formatMoney(item.amountString,'',item.currency)}</samp></p>`;
+
+                        html += `<div class="list ${item.currency.toLocaleLowerCase()}">
 							<p>${item.currency}</p>
 							<p class="amount">${base.formatMoneySubtract(item.amountString,item.frozenAmountString,item.currency)}</p>
 							<p class="frozenAmountString">${base.formatMoney(item.frozenAmountString,'',item.currency)}</p>
 						</div>`;
-	    			}
-    			}
-    		})
-    		if(data.accountList.length>=3){
-    				htmlAccount +=`<p class="more">查看更多</p>`;
-	    			html += `<div class="list more">查看更多</div>`;
-    			}
-    		$("#head-user-wrap .wallet .wallet-account-wrap").html(htmlAccount);
-    		$("#head-user-wrap .wallet .wallet-account-mx .listWrap").html(html)
-    	},base.hideLoadingSpin)
+                    }
+                }
+            })
+            if(data.accountList.length>=3){
+                htmlAccount +=`<p class="more">查看更多</p>`;
+                html += `<div class="list more">查看更多</div>`;
+            }
+            $("#head-user-wrap .wallet .wallet-account-wrap").html(htmlAccount);
+            $("#head-user-wrap .wallet .wallet-account-mx .listWrap").html(html)
+        },base.hideLoadingSpin)
     }
-    
+
     function addListener(){
-    	
-    	$("#headLogout").click(function(){
-    		base.logout()
-    	})
-    	$(".am-modal-mask").click(function(){
-    		$(this).parent(".dialog").addClass("hidden")
-    	})
-    	
-    	$("#head .head-nav-wrap .advertise .goHref").off("click").click(function(){
-    		if(!base.isLogin()){
-	    		base.goLogin();
-	    		return false;
-	    	}else{
-    		    base.showLoadingSpin();
+
+        $("#headLogout").click(function(){
+            base.logout()
+        })
+        $(".am-modal-mask").click(function(){
+            $(this).parent(".dialog").addClass("hidden")
+        })
+
+        $("#head .head-nav-wrap .advertise .coinItem").on('click', function(){
+            if(!base.isLogin()){
+                base.goLogin();
+                return false;
+            }else{
+                base.showLoadingSpin();
                 UserCtr.getUser().then((data) => {
                     base.hideLoadingSpin();
+                    var thishref = $(this).attr("data-href");
                     if (data.zfbAccount) {
-                        var thishref = $(this).attr("data-href");
                         base.gohref(thishref)
                     } else if (!data.zfbAccount) {
                         base.showMsg("請先設置支付寶賬號及付款碼")
-                        sessionStorage.setItem("l-return", location.href);
+                        sessionStorage.setItem("l-return", thishref);
                         setTimeout(function () {
                             base.gohref("../user/setPayQRCode.html")
                         }, 1200)
                     }
                 }, base.hideLoadingSpin)
-	    	}
-    	})
+            }
+        })
     	
     	$("#head .head-nav-wrap .invitation").off("click").click(function(){
     		if(!base.isLogin()){
